1712055b37d23397306fb26d1152ccc0c1580b75
Dmitry Gulitsky <mrdmzver@gmail.com>
2019-01-03 00:06:16 +0300

    Add comments

--- redux-basics-redux-intro/src/redux.js
+++ redux-basics-redux-intro/src/redux.js
@@ -1,17 +1,20 @@
-export function createStore(reducer, initialState) {   // reducer - функция, изменяющая состояние. initialState - начальное состояние
+export function createStore(reducer, initialState) {
+  // функция для создания хранилища
+  // reducer - функция, изменяющая состояние.
+  // initialState - начальное состояние
   let state = initialState;   // переменная для хранения состояния
   let callbacks = []; // переменная для хранения функции обратного вызова
 
   const getState = () => state;   // функция, которая вовращает текущее состояние
 
-  const dispatch = action => {
-    state = reducer(state, action);   //  получим новое состояние
+  const dispatch = action => {  // функция, которая изменяет состояние. принимает аргумент - действие
+    state = reducer(state, action);   //  получим новое состояние, вызывая функция reducer
     callbacks.forEach(callback => callback()); // перебираем массив, сообщаем каждой из функций
   };
 
-  const subscribe = callback => {
-    callbacks.push(callback);
-    return () => callbacks.filter(cb => cb !== callback);
+  const subscribe = callback => {   //  подписка на состояние
+    callbacks.push(callback);     // в массив добавим переданную функцию
+    return () => callbacks.filter(cb => cb !== callback);    // filter вернет новый массив без функции, передоваемов в метод subscribe. Для этого проверяем не является ли очередная функция массива переданной в метод subscribe
   };
 
   dispatch({}); // отправим пустой объект как начальное состояние
--- redux-basics-redux-intro/src/store.js
+++ redux-basics-redux-intro/src/store.js
@@ -1,8 +1,8 @@
 export default class Store {
-  constructor(updateState, state) {
+  constructor(updateState, state) {   // первый аргумент - функция
     this._updateState = updateState;
     this._state = state;
-    this._callbacks = [];   //    хранилище функций
+    this._callbacks = [];   //    хранилище списка функций
   }
 
   get state() {       //  возвращаем текущее состояние
@@ -10,12 +10,12 @@ export default class Store {
   }
 
   update(action) {        // функция, которая имеет возможность изменять состояния. action - объект действия. Просто уведомляем о событии хранилище, ничего не получая в ответ
-    this._state = this._updateState(this._state, action);       // this._state - текущее состояние
-    this._callbacks.forEach(callback => callback());      // перебрать массив метожов и вызвать каждую функцию
+    this._state = this._updateState(this._state, action);       // this._state - текущее состояние, action - объект действия
+    this._callbacks.forEach(callback => callback());      // перебрать массив методов и вызвать каждую функцию
   }
 
-  subscribe(callback) {
-    this._callbacks.push(callback);     // в хранилище добавим переданную функцию
+  subscribe(callback) {   // метод с помощью которого можно подписаться на обновления состояний хранилища
+    this._callbacks.push(callback);     // в массив добавим переданную функцию
     return () => this._callbacks = this._callbacks.filter(cb => cb !== callback);    // filter вернет новый массив без функции, передоваемов в метод subscribe. Для этого проверяем не является ли очередная функция массива переданной в метод subscribe
   }
 }
